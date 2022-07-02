51b41f5c6a1a9105adaaa2f86759d11eeee160cf
Muhammad Yazid Akbar <91299044+Yazz803@users.noreply.github.com>
2022-07-02 12:33:14 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,5 @@
 
 $conn = mysqli_connect("localhost", "root", "", "compress_img");
 
-// $conn = mysqli_connect("sql206.epizy.com", "epiz_31693632", "7Tcijf5Hx0L", "epiz_31693632_secreatmsg");
 
 ?>
